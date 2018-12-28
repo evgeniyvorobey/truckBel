b347906af256525653877c63f16502086a6bc4df
evgeniyvorobey <31759882+evgeniyvorobey@users.noreply.github.com>
2018-12-28 13:12:04 +0200

    header fix

--- webpack.config.js
+++ webpack.config.js
@@ -103,10 +103,6 @@ module.exports = {
         to: './fonts'
       },
       {
-        from: './src/favicon',
-        to: './favicon'
-      },
-      {
         from: './src/img',
         to: './img'
       },
